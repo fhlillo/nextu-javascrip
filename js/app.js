c7bc730d8b63cdbe65dc5c6d225733154d605492
fhlillo <f.guicharrousse@gmail.com>
2017-04-08 22:15:54 -0300

    se inicializa objetos

--- js/app.js
+++ js/app.js
@@ -1,23 +1,36 @@
 var Calculadora = {
+	/* Declaración de variable*/
+	pantalla: document.getElementById("display").innerHTML,
+	inicio: (
+		function(){
+			this.EventosClick();
+		}
+	),
+	
 	/* Se asignan teclas */
 	EventosClick: function(){
-		document.getElementById("0").addEventListener("Clic",function(){Calculadora.viewnum("0")});
-		document.getElementById("1").addEventListener("Clic",function(){Calculadora.viewnum("1")});
-		document.getElementById("2").addEventListener("Clic",function(){Calculadora.viewnum("2")});
-		document.getElementById("3").addEventListener("Clic",function(){Calculadora.viewnum("3")});
-		document.getElementById("4").addEventListener("Clic",function(){Calculadora.viewnum("4")});
-		document.getElementById("5").addEventListener("Clic",function(){Calculadora.viewnum("5")});
-		document.getElementById("6").addEventListener("Clic",function(){Calculadora.viewnum("6")});
-		document.getElementById("7").addEventListener("Clic",function(){Calculadora.viewnum("7")});
-		document.getElementById("8").addEventListener("Clic",function(){Calculadora.viewnum("8")});
-		document.getElementById("9").addEventListener("Clic",function(){Calculadora.viewnum("9")});
-		document.getElementById("on").addEventListener("Clic",function(){Calculadora.viewnum("")});
-		document.getElementById("sign").addEventListener("Clic",function(){Calculadora.sign()});
-		document.getElementById("dividido").addEventListener("Clic",function(){Calculadora.dividido()});
-		document.getElementById("menos").addEventListener("Clic",function(){Calculadora.menos()});
-		document.getElementById("punto").addEventListener("Clic",function(){Calculadora.punto()});
-		document.getElementById("igual").addEventListener("Clic",function(){Calculadora.igual()});
-		document.getElementById("mas").addEventListener("Clic",function(){Calculadora.mas()});
+		document.getElementById("0").addEventListener("click",function(){Calculadora.viewnum("0")});
+		document.getElementById("1").addEventListener("click",function(){Calculadora.viewnum("1")});
+		document.getElementById("2").addEventListener("click",function(){Calculadora.viewnum("2")});
+		document.getElementById("3").addEventListener("click",function(){Calculadora.viewnum("3")});
+		document.getElementById("4").addEventListener("click",function(){Calculadora.viewnum("4")});
+		document.getElementById("5").addEventListener("click",function(){Calculadora.viewnum("5")});
+		document.getElementById("6").addEventListener("click",function(){Calculadora.viewnum("6")});
+		document.getElementById("7").addEventListener("click",function(){Calculadora.viewnum("7")});
+		document.getElementById("8").addEventListener("click",function(){Calculadora.viewnum("8")});
+		document.getElementById("9").addEventListener("click",function(){Calculadora.viewnum("9")});
+		document.getElementById("on").addEventListener("click",function(){Calculadora.viewnum("")});
+		document.getElementById("sign").addEventListener("click",function(){Calculadora.sign()});
+		document.getElementById("dividido").addEventListener("click",function(){Calculadora.dividido()});
+		document.getElementById("menos").addEventListener("click",function(){Calculadora.menos()});
+		document.getElementById("punto").addEventListener("click",function(){Calculadora.punto()});
+		document.getElementById("igual").addEventListener("click",function(){Calculadora.igual()});
+		document.getElementById("mas").addEventListener("click",function(){Calculadora.mas()});
 		
+	},
+	/* función vision de calculadora*/
+	viewnum: function(valor){
+		alert(valor);
 	}
 }
+Calculadora.inicio();
